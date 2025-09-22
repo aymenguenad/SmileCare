25800a50f956748ed24c21e66beac215c6e191b0
Aymene Guenad <aymene.guenad@icloud.com>
2025-09-22 05:16:07 +0100

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), {
-    name: 'copy-404',
-    closeBundle () {
-      copyFileSync (
-        resolve(__dirname, 'dist/index.html'),
-        resolve(__dirname, 'dist/404.html')
-      )
-    }
-  }],
+  plugins: [
+    react(),
+    {
+      name: 'copy-404',
+      closeBundle() {
+        copyFileSync(
+          resolve(__dirname, 'dist/index.html'),
+          resolve(__dirname, 'dist/404.html')
+        )
+      },
+    },
+  ],
   base: '/SmileCare/'
 })
