99eb09c2bfdfcf6e81fcec789b938d5b244fec18
Aymene Guenad <aymene.guenad@icloud.com>
2025-09-22 05:01:04 +0100

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
       {screenWidth.initial > 600 || screenWidth.change > 600  ? '' : <MyMenu />}
       <main>
         <Routes>
-          <Route path="/" element={<HomePage />}/>
+          <Route path="/SmileCare/" element={<HomePage />}/>
         </Routes>
       </main>
       <footer>
